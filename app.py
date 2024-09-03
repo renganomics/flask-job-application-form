5ac1420368621a6716a4d2118eb547531663b6fa
renganomics <orengasi@gmail.com>
2024-09-03 23:10:39 +0100

    Added email notification

--- app.py
+++ app.py
@@ -3,12 +3,22 @@ import os
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request, flash
+from flask_mail import Mail, Message
+
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.environ["FLASK_PASS"]
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "orengsapptester@gmail.com"
+app.config["MAIL_PASSWORD"] = os.environ["GMAIL_APP_TESTER_PASSWORD"]
+
 db = SQLAlchemy(app)
+
+mail = Mail(app)
 
 
 class Form(db.Model):
@@ -36,6 +46,16 @@ def index():
                     email=email, date=date_object, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+
+        message_body = f"Thank you for your submission {first_name}! " \
+                       f"Here is your data:\n{first_name} {last_name}\n{date}\n" \
+                       f"Thank you!"
+        message = Message("New form submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+        mail.send(message)
+
         flash(f"{first_name}, your form was submitted successfully!", "success")
 
     return render_template("index.html")
