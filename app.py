c4f5f8fce6cea768743ff3b22d44686d4de489a4
renganomics <orengasi@gmail.com>
2024-09-03 19:43:31 +0100

    Added values to radio buttons and requested form data from webpage

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    print(request.method)
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+
     return render_template("index.html")
 
 
-app.run(debug=True, port=5001)
+app.run(debug=True, port=8080)
